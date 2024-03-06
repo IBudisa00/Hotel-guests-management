650a1742d2109aae266a319a4123f1cdef37e5c2
IBudisa00 <73231557+IBudisa00@users.noreply.github.com>
2024-03-06 11:20:58 +0100

    Add files via upload

--- application.cc
+++ application.cc
@@ -2,31 +2,56 @@
 #include <management.hh>
 #include <map>
 #include <utility>
+#include <fstream>
+#include <string>
+
+void registerGuestsFromFile(hotel &hotel);
+void registerGuests(hotel& hotel, char *firstName, char *lastName, unsigned int daysToStay);
 
 int main(){
     char hotelName[MAX_SIZE];
-    std::map<int, std::pair<char[], char[]>> guests;
 
-    fillGuestMap(guests);
     std::cout << "Hotel's name: ";
     std::cin >> hotelName;
     hotel hotel(hotelName);
 
-    registerGuests(hotel,guests);
+    registerGuestsFromFile(hotel);
+
     return 0;
 }
 
-void fillGuestMap(std::map<int, std::pair<char[], char[]>> &guestMap){
-    //read file with data
+void registerGuestsFromFile(hotel &hotel){
+    //file structure is name surname daysToStay
+    std::ifstream guestsInfoFile;
+    char guestInput[MAX_INPUT];
+    std::string inputLine;
+    char firstName[MAX_SIZE];
+    char lastName[MAX_SIZE];
+    unsigned int daysToStay;
+
+    guestsInfoFile.open("guests.txt");
+
+    if(!guestsInfoFile.is_open())
+    {
+        std::cout << "ERROR! FILE NOT OPENED PROPERLY.\n";
+        return;
+    }
+
+    while(!guestsInfoFile.eof())
+    {
+        std::getline(guestsInfoFile, inputLine);
+        while(std::getline(guestsInfoFile, inputLine, ' ') != "\n")
+        {
+            strcpy(firstName, inputLine.c_str());
+            strcpy(lastName, inputLine.c_str());
+            daysToStay = inputLine;
+            registerGuests(hotel, firstName, lastName, daysToStay);
+        }
+    }
+
+    guestsInfoFile.close();
 }
 
-void registerGuests(hotel &hotel, std::map<int, std::pair<char[], char[]>> &guestMap){
-    std::map<int, std::pair<char[], char[]>>::iterator it = guestMap.begin();
-
-    while(it != guestMap.end())
-    {
-        //change guestMap sturcutre of map
-        hotel.addNewGuest();
-        ++it;
-    }
+void registerGuests(hotel& hotel, char *firstName, char *lastName, unsigned int daysToStay){
+    hotel.addNewGuest(firstName, lastName, daysToStay);
 }
--- defines.hh
+++ defines.hh
@@ -1,5 +1,6 @@
 #define MAX_SIZE 20
 #define MAX_ROOMS 15
+#define MAX_INPUT 30
 
 #define SMALL 1
 #define MEDIUM 2
